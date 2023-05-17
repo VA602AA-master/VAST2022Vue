15dfbf9486ca4747bdd0af9e6e6ca2f8ecf6b266
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2023-05-17 15:46:09 +0200

    fixing the correct URL for endpoint

--- src/assets/api_connector.js
+++ src/assets/api_connector.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3000/api/v1';
+const BASE_URL = 'http://kddrtserver3.isti.cnr.it:8000';
 
 const apiConnector = axios.create({
     baseURL: BASE_URL,
